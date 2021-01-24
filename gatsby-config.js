3bf7caff55a3ffeeffad0487ab2b5d2e8bcef3fa
Michael-Hanley <pasaquali@gmail.com>
2021-01-23 17:02:30 -0800

    Add gtracking id

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,18 +1,20 @@
 module.exports = {
   siteMetadata: {
-    title: "A Deck of Negatives",
+    title: "A Deck of Negatives",    
+    siteUrl: `https://www.deckofnegatives.com`,
+    description: `A game of lies and deceit`,
   },
   plugins: [
     "gatsby-plugin-sass",
-    // {
-    //   // resolve: "gatsby-plugin-google-analytics",
-    //   // options: {
-    //   //   trackingId: "",
-    //   // },
-    // },
+    {
+      resolve: "gatsby-plugin-google-analytics",
+      options: {
+        trackingId: "G-KWZYQ37CDC",
+      },
+    },
     "gatsby-plugin-sharp",
     "gatsby-plugin-react-helmet",
-    // "gatsby-plugin-sitemap",
+    "gatsby-plugin-sitemap",
     "gatsby-transformer-sharp",
     {
       resolve: "gatsby-source-filesystem",
